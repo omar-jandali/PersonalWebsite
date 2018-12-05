0e82407691c7c25d00b80d8e1f5f6d86c2686596
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-12-05 11:47:55 -0800

    setup the inital server

---
+++ index/bin/www/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express()
+
+app.use(cors)
+app.use(morgan('dev'));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../../../client/dist')))
+
+module.exports = app;
---
+++ index/index.js
@@ -0,0 +1,6 @@
+const app = require('./bin/www/app');
+require('dotenv').config();
+
+const port = process.env.PORT || 1221;
+
+app.listen(port, console.log(`server is running on port ${port}`));
