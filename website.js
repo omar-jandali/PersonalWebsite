e8dea0961983c99ab2d929ef0540f2ceb187309c
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-12-05 12:24:10 -0800

    inserted non dynamic data into a javascript file

---
+++ website.js
@@ -0,0 +1,21 @@
+const skillsLanguages = ['Javascript', 'Python']
+const skillsFrameWorks = ['React', 'Angular', 'Django', 'Flask']
+const SkillsFrontEnd = ['ES6 & ES5', 'HTML5', 'CSS3', 'Bootstrap', 'Google Materials']
+const skillsBackend = ['NodeJs native', 'Express', 'SQL', 'PostgreSQL', 'MySQL', 'NoSQL', 'MongoDB']
+const skillsDeloyment = ['Docker', 'AWS EC2', 'AWS S3', 'Digital Ocean']
+const skillsTesting = ['TDD', 'Jest', 'ESLint', 'Mocha']
+const skillsOther = ['RESTful', 'Agile', 'Scrum', 'communication', 'API integration', 'OOP']
+
+const contact = {
+  first_name: 'Omar',
+  last_name: 'Jandali',
+  full_name: 'Omar Jandali',
+  location: 'Mission Viejo, CA',
+  phone: '(951) 534-3666',
+  email: 'omarjandali@omnacore.com',
+  github: 'https://github.com/omar-jandali',
+  linkedin: 'https://www.linkedin.com/in/itsmeomarj/',
+  twitter: 'https://www.twitter.com/itsmeomarj',
+  instagram: 'http://www.instagram.com/itsmeomarj',
+  position: 'Software Engineer (Javascript)'
+}
