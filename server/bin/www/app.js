ba8ec794728c09aa2bbf719625a81ef8d46c2401
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-12-06 12:09:02 -0800

    fixed issue with React rendering

--- server/bin/www/app.js
+++ server/bin/www/app.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const path = require('path');
-const parser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express")
+const path = require("path")
+const morgan = require("morgan")
+const parser = require("body-parser")
+var cors = require('cors')
 const routerEducation = require('../../routers/routerEducation');
 const routerExperience = require('../../routers/routerExperience');
 const routerApplication = require('../../routers/routerApplication');
 require('dotenv').config();
-require('../../../database/index');
 
 const app = express();
 
-app.use(cors)
+app.use(cors())
 app.use(morgan('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../../client/dist')));
+app.use(express.static(path.join(__dirname, "../../../client/dist")));
 
 app.use('/education', routerEducation);
 app.use('/experience', routerExperience);
