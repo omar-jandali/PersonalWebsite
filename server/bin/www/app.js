94e0d6395fac75af4739ab196180456accf10bdd
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-12-05 16:43:33 -0800

    integrated the different routers for education and expereince

--- server/bin/www/app.js
+++ server/bin/www/app.js
@@ -3,15 +3,20 @@ const path = require('path');
 const parser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
+const routerEducation = require('../../routers/routerEducation');
+const routerExperience = require('../../routers/routerExperience');
 require('dotenv').config();
-require('../../../database/index')
+require('../../../database/index');
 
-const app = express()
+const app = express();
 
 app.use(cors)
 app.use(morgan('dev'));
 app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../../../client/dist')))
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../../../client/dist')));
+
+app.use('/education', routerEducation);
+app.use('/experience', routerExperience);
 
 module.exports = app;
