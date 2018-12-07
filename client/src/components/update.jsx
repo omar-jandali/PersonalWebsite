f079ad7923e14d9bad8b6704f00df45a459ea2de
omar jandali <omarjandali@omars-MBP.attlocal.net>
2018-12-06 23:37:57 -0800

    added all the create forms for education, experience, and applications

--- client/src/components/update.jsx
+++ client/src/components/update.jsx
@@ -21,7 +21,55 @@ export default function Update(props){
           image:
           <input type="text" name="image"/>
         </label><br/>
-        <input type="submit" name="submit" value="submit"/>
+        <input type="submit" name="submit" value="Submit"/>
+      </form>
+      <form>
+        <h3>Application</h3>
+        <label>
+          Name:
+          <input type="text" name="name"/>
+        </label><br/>
+        <label>
+          Image:
+          <input type="text" name="image"/>
+        </label><br/>
+        <label>
+          tools (seperate by a comma):
+          <input type="number" name="tools"/>
+        </label><br/>
+        <label>
+          Description:
+          <input type="text" name="description"/>
+        </label><br/>
+        <label>
+          Github:
+          <input type="text" name="github"/>
+        </label><br/>
+        <input type="submit" name="submit" value="Submit"/>
+      </form>
+      <form>
+        <h3>Experience</h3>
+        <label>
+          Position:
+          <input type="text" name="position"/>
+        </label><br/>
+        <label>
+          Company:
+          <input type="text" name="company"/>
+        </label><br/>
+        <label>
+          Description:
+          <input type="number" name="description"/>
+        </label><br/>
+        <label>
+          From (Year):
+          <input type="text" name="from"/>
+        </label><br/>
+        <label>
+          To (Year):
+          <input type="text" name="to"/>
+        </label><br/>
+        <input type="submit" name="submit" value="Submit"/>
       </form>
     </div>
   )
